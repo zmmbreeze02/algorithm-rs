d3879022165912e56cf2c7ec5ddd3e793baf9806
zmmbreeze02 <zmmbreeze@gmail.com>
2023-08-15 21:50:46 +0800

    feat: added ringbuffer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 pub mod binary_search;
 pub mod binary_search_left;
 pub mod binary_search_right;
-// pub mod ringbuffer;
+pub mod ringbuffer;
+pub mod atomic;
 
 use std::{alloc::{alloc, Layout}, ptr, cell::RefCell};
 
@@ -17,46 +18,49 @@ unsafe fn get_val() -> *const i32 {
 }
 
 fn main() {
-    let mut tree_root = TreeNode {
-        value: Option::Some(1),
-        children: Vec::new(),
-        parent: Option::None,
-    };
-    // tree_root.children.push(TreeNode { value: Some(2), children: Vec::new(), parent: Some(&tree_root) });
+    // let mut tree_root = TreeNode {
+    //     value: Option::Some(1),
+    //     children: Vec::new(),
+    //     parent: Option::None,
+    // };
+    // // tree_root.children.push(TreeNode { value: Some(2), children: Vec::new(), parent: Some(&tree_root) });
 
-    println!("Hello world");
-    let layout = Layout::array::<u32>(4).unwrap();
-    unsafe {
-        let buf = alloc(layout) as *mut u32;
-        ptr::write(buf, 1);
-        ptr::write(buf.wrapping_add(1), 2);
-        ptr::write(buf.wrapping_add(2), 3);
-        ptr::write(buf.wrapping_add(3), 4);
-        ptr::write(buf.add(4), 5);
-        println!("{}", *buf);
-        println!("{}", *(buf.add(1)));
-    }
+    // println!("Hello world");
+    // let layout = Layout::array::<u32>(4).unwrap();
+    // unsafe {
+    //     let buf = alloc(layout) as *mut u32;
+    //     ptr::write(buf, 1);
+    //     ptr::write(buf.wrapping_add(1), 2);
+    //     ptr::write(buf.wrapping_add(2), 3);
+    //     ptr::write(buf.wrapping_add(3), 4);
+    //     ptr::write(buf.add(4), 5);
+    //     println!("{}", *buf);
+    //     println!("{}", *(buf.add(1)));
+    // }
 
-    let data1 = String::from("Hello");
-    // let a: &String = &data1;
-    // let mut data2 = String::from("World");
-    // let b: &mut String = &mut data2;
-    let a: &str = data1.as_ref();
+    // let data1 = String::from("Hello");
+    // // let a: &String = &data1;
+    // // let mut data2 = String::from("World");
+    // // let b: &mut String = &mut data2;
+    // let a: &str = data1.as_ref();
 
 
-    let mut my_num: Box<i32> = Box::new(10);
-    let ref1 = my_num.as_ref();
-    // let ref2 = my_num.as_mut();
-    // *ref2 = 3;
-    // let ref3 = &mut *my_num;
-    let my_num_ptr: *const i32 = &*my_num;
-    // let my_num_ptr = Box::into_raw(my_num);
-    unsafe {
-        println!("> {ref1} {} {}", *my_num_ptr, my_num);
-        println!("> {}", *get_val());
-    }
+    // let mut my_num: Box<i32> = Box::new(10);
+    // let ref1 = my_num.as_ref();
+    // // let ref2 = my_num.as_mut();
+    // // *ref2 = 3;
+    // // let ref3 = &mut *my_num;
+    // let my_num_ptr: *const i32 = &*my_num;
+    // // let my_num_ptr = Box::into_raw(my_num);
+    // unsafe {
+    //     println!("> {ref1} {} {}", *my_num_ptr, my_num);
+    //     println!("> {}", *get_val());
+    // }
 
-    let my = Box::new(1);
-    let aa = &my;
-    println!("{aa}");
+    // let my = Box::new(1);
+    // let aa = &my;
+    // println!("{aa}");
+
+    // atomic::test1();
+    
 }
--- src/ringbuffer.rs
+++ src/ringbuffer.rs
@@ -1,10 +1,23 @@
-use std::alloc::{alloc, Layout};
+use std::{alloc::{alloc, Layout}, ptr};
 
 
+/**
+ * RingBuffer implementation
+ * 
+ * | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 |
+ *       | head                           | tail
+ * read from head, and write after tail
+ */
 pub struct RingBuffer<T> {
     buf: *mut T,
+    // Buffer size
+    // It must be a power of two.
+    // When convert head position into index of buffer, it will become much easier.
+    // Can't be modified in runtime.
     capacity: usize,
+    // head position, read from head
     head: usize,
+    // tail position, write after tail
     tail: usize,
 }
 
@@ -25,8 +38,32 @@ impl<T> RingBuffer<T> {
         }
     }
 
-    pub fn push(&mut self, value: T) {
+    /// Turn position(head and tail) into index with offset.
+    fn position_to_index(cap: usize, position: usize) -> usize {
+        position & (cap - 1)
+    }
 
+    /// Push new element after tail position.
+    /// If it is full, then pop the head element, and push the new element.
+    pub fn push(&mut self, value: T) {
+        if self.is_full() {
+            // Manually drop the head element
+            unsafe {
+                let index = Self::position_to_index(self.capacity, self.head);
+                let old_value = ptr::read(self.buf.add(index));
+                drop(old_value);
+            }
+
+            self.head += 1;
+        }
+
+        // Calculate the index to push
+        let index = Self::position_to_index(self.capacity, self.tail);
+        unsafe {
+            ptr::write(self.buf.add(index), value);
+        }
+
+        self.tail += 1;
     }
 
     pub fn enqueue(&mut self, value: T) {
@@ -34,30 +71,89 @@ impl<T> RingBuffer<T> {
     }
 
     pub fn dequeue(&mut self) -> Option<T> {
-        Option::None
+        if self.is_empty() {
+            return None;
+        }
+
+        let index = Self::position_to_index(self.capacity, self.head);
+        let p = unsafe {
+            self.buf.add(index)
+        };
+        self.head += 1;
+
+        unsafe {
+            Some(ptr::read(p))
+        }
     }
 
     /// Empties the buffer entirely. Sets the length to 0 but keeps the capacity allocated.
     pub fn clear(&mut self) {
-
+        self.head = 0;
+        self.tail = 0;
     }
 
-    /// Gets a value relative to the current index. 0 is the next index to be written to with push.
-    /// -1 and down are the last elements pushed and 0 and up are the items that were pushed the longest ago.
+    /// Turn position(head and tail) into index with offset.
+    /// When offset is [-1, `-len`], get the last elements pushed index.
+    /// When offset is [0, `len - 1`], get the items that were pushed the longest ago.
+    fn position_to_index_with_offset(cap: usize, position: usize, len: usize, offset: isize) -> Option<usize> {
+        let real_offset: isize = 
+            if offset >= 0 { offset }
+            else { len as isize + offset };
+
+        if real_offset < 0 || real_offset > len as isize {
+            return None;
+        }
+        
+        Some(Self::position_to_index(cap, position) + real_offset as usize)
+    }
+
+    /// Gets a value relative to the current index.
+    /// -1 and down are the last elements pushed.
+    /// 0 and up are the items that were pushed the longest ago.
     pub fn get(&self, index: isize) -> Option<&T> {
-        Option::None
+        if self.is_empty() {
+            return None;
+        }
+
+        let index = Self::position_to_index_with_offset(
+            self.capacity,
+            self.head,
+            self.len(),
+            index
+        );
+        index.map(|i| {
+            unsafe {
+                // Move pointer to specified element
+                let p = self.buf.add(i);
+                &*p
+            }
+        })
     }
 
-    /// Gets a value relative to the current index mutably. 0 is the next index to be written to with push.
-    /// -1 and down are the last elements pushed and 0 and up are the items that were pushed the longest ago.
-    #[inline]
+    /// Gets a value relative to the current index mutably.
+    /// -1 and down are the last elements pushed.
+    /// 0 and up are the items that were pushed the longest ago.
     pub fn get_mut(&mut self, index: isize) -> Option<&mut T> {
-        // Safety: self is a RingBuffer
-        unsafe { Self::ptr_get_mut(self, index).map(|i| &mut *i) }
+        if self.is_empty() {
+            return None;
+        }
+        let index = Self::position_to_index_with_offset(
+            self.capacity,
+            self.head,
+            self.len(),
+            index
+        );
+        index.map(|i| {
+            unsafe {
+                // Move pointer to specified element
+                let p = self.buf.add(i);
+                &mut *p
+            }
+        })
     }
 
     pub fn len(&self) -> usize {
-        0
+        self.tail.wrapping_sub(self.head)
     }
 
     pub fn is_empty(&self) -> bool {
@@ -67,7 +163,5 @@ impl<T> RingBuffer<T> {
     pub fn is_full(&self) -> bool {
         self.len() == self.capacity
     }
-
-
 }
 
